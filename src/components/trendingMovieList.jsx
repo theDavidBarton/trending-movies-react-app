b2276dfe985121150b9067038a11d1623391de3f
David Barton <43063460+theDavidBarton@users.noreply.github.com>
2019-08-15 12:51:13 +0200

    upload project (#1)
    * create react app upload project
    
    * remove unneccesary files

---
+++ src/components/trendingMovieList.jsx
@@ -0,0 +1,34 @@
+import React, { Component } from 'react'
+import TrendingMovie from './trendingMovie'
+
+class TrendingMovieList extends Component {
+  state = {
+    topMovieCount: [
+      { id: 1, value: 0 },
+      { id: 2, value: 1 },
+      { id: 3, value: 2 },
+      { id: 4, value: 3 },
+      { id: 5, value: 4 },
+      { id: 6, value: 5 },
+      { id: 7, value: 6 },
+      { id: 8, value: 7 },
+      { id: 9, value: 8 }
+    ]
+  }
+
+  render() {
+    return (
+      <div className="badge-dark" style={{ paddingBottom: '50px', paddingTop: '25px' }}>
+        <div className="container">
+          <div className="row">
+            {this.state.topMovieCount.map(movies => (
+              <TrendingMovie key={movies.id} value={movies.value} />
+            ))}
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default TrendingMovieList
---
+++ src/index.js
@@ -0,0 +1,6 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import 'bootstrap/dist/css/bootstrap.css'
+import HomepageWrapper from './components/homepageWrapper'
+
+ReactDOM.render(<HomepageWrapper />, document.getElementById('root'))
