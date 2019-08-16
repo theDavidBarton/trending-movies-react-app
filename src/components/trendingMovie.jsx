3e3efce6abaee770d5630c8cbc0985ce2c5bf613
David Barton <barton.david.sv@gmail.com>
2019-08-16 21:44:07 +0200

    add ratings

--- src/components/trendingMovie.jsx
+++ src/components/trendingMovie.jsx
@@ -32,18 +32,23 @@ class TrendingMovie extends Component {
     return overview
   }
 
+  getRating = () => {
+    const rating = this.state.response[this.state.value].vote_average
+    return rating
+  }
+
   render() {
     return (
       <div className="col">
         <div className="card badge-dark shadow-lg" style={{ width: '342px' }}>
           {this.parseJson()}
-          <div>
+          <div><div class='badge-pill badge-warning' style={{ position: 'absolute' }}>★{this.getRating()}/10</div>
             <img className="card-img-top" alt="movie poster" src={this.getPoster()} />
           </div>
           <div className="card-body">
             <h2 className="display-4">{this.getTitle()}</h2>
             <p>{this.getOverview()}</p>
-            <a type="button" className="btn btn-warning" href={this.getLink()}>
+            <a className="btn btn-warning btn-sm" href={this.getLink()}>
               Check out {this.getTitle()}!
             </a>
           </div>
