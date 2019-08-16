244ce0fc0d3b5f385bac38923b24205504559559
David Barton <43063460+theDavidBarton@users.noreply.github.com>
2019-08-16 19:05:44 +0200

    "put that cookie down! NOW!" (#13)

--- src/components/cookieBar.jsx
+++ src/components/cookieBar.jsx
@@ -1,16 +1,28 @@
 import React, { Component } from 'react'
 
 class CookieBar extends Component {
+  state = {
+    isOpened: true
+  }
+
+  closeIt = () => {
+    this.setState({ isOpened: false })
+  }
+
   render() {
     return (
-      <div className="badge-warning sticky-top" style={{ paddingBottom: '10px', paddingTop: '10px' }}>
-        <div className="container">
+      <React.Fragment>
+        { this.state.isOpened ?
+          <div className="badge-warning sticky-top" style={{ paddingBottom: '10px', paddingTop: '10px' }}>
+            <div className="container">
         This page doesn't store cookies, but TMDb does. Check out <a className="text-success" href="https://www.themoviedb.org/privacy-policy">their cookie policy</a> to opt out!
-          <button type="button" className="close" aria-label="Close">
-            <span aria-hidden="true">&times;</span>
-          </button>
-        </div>
-      </div>
+              <button onClick={this.closeIt} type="button" className="close" aria-label="Close">
+                <span aria-hidden="true">&times;</span>
+              </button>
+            </div>
+          </div>
+          :null }
+      </React.Fragment>
     )
   }
 }
