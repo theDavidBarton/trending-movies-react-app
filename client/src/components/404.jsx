86cd59ae3833bda8dd9bdf46447193cab604762d
David Barton <barton.david.sv@gmail.com>
2019-11-25 23:16:32 +0100

    replace png logos by svg and facelift header and footer

--- client/src/components/404.jsx
+++ client/src/components/404.jsx
@@ -1,21 +1,26 @@
 import React, { Component, Fragment } from 'react'
-import logoDark from './../img/logo-big-dark.png'
+import logoDark from './../img/logo-big-dark.svg'
 
 class Page404 extends Component {
   render() {
     return (
       <Fragment>
         <div className='bg-dark text-warning'>
-          <div className='container'>
+          <div className='container text-center'>
             <div className='row'>
-              <h1 className='col'>404 not found</h1>
+              <h1 className='col display-2 py-5'>404 not found</h1>
             </div>
           </div>
         </div>
-        <div className='bg-white container'>
-          <img className='row text-center' alt='logo' src={logoDark} />
-          <p className='col'>
-            Back to <a href='/'> home</a>.
+        <div className='bg-white container text-center'>
+          <img className='col img-fluid py-5' src={logoDark} alt='logo' style={{ height: '100%', width: '350px' }} />
+          <p className='col display-4'>
+            Back to{' '}
+            <a className='text-secondary' href='/'>
+              {' '}
+              home
+            </a>
+            .
           </p>{' '}
         </div>
       </Fragment>
