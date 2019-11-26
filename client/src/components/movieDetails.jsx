ea5f4bf7782f16fdebee9347bc7a528f1d8137de
David Barton <43063460+theDavidBarton@users.noreply.github.com>
2019-11-26 23:09:35 +0100

    extend detail page (#49)
    * extend detail page w lot of fun stuff
    
    * refactor and cleanup code

--- client/src/components/movieDetails.jsx
+++ client/src/components/movieDetails.jsx
@@ -5,7 +5,6 @@ class MovieDetails extends Component {
     data: null,
     dataIsReady: false,
     id: this.props.selectedMovie,
-    isOpened: true,
     displayedCastMembers: 5,
     fullCastIsOpened: false
   }
@@ -35,6 +34,11 @@ class MovieDetails extends Component {
     return releaseYear
   }
 
+  getReleaseDate = () => {
+    const releaseDate = this.state.data.release_date
+    return releaseDate
+  }
+
   getTagline = () => {
     const tagline = this.state.data.tagline
     return tagline
@@ -48,6 +52,11 @@ class MovieDetails extends Component {
   getRuntime = () => {
     const runtime = this.state.data.runtime
     return runtime
+  }
+
+  getVotes = () => {
+    const votes = this.state.data.vote_average
+    return votes
   }
 
   getGenres = () => {
@@ -88,11 +97,41 @@ class MovieDetails extends Component {
     return poster
   }
 
+  getCrew = () => {
+    const castImageBase = 'https://image.tmdb.org/t/p/w90_and_h90_face'
+    const directorArray = this.state.data.credits.crew.filter(crewMember => crewMember.job === 'Director')
+    const writerArray = this.state.data.credits.crew.filter(crewMember => crewMember.job === 'Writer')
+    const novelWriterArray = this.state.data.credits.crew.filter(crewMember => crewMember.job === 'Novel')
+    const screenWriterArray = this.state.data.credits.crew.filter(crewMember => crewMember.job === 'Screenplay')
+    const importantCrewArray = [...directorArray, ...writerArray, ...novelWriterArray, ...screenWriterArray]
+    const importantCrewMembers = importantCrewArray.map(crewMember => (
+      <Fragment key={crewMember.id + crewMember.job}>
+        <li className='col media my-3'>
+          {crewMember.profile_path ? (
+            <img alt={crewMember.name} src={castImageBase + crewMember.profile_path} className='mr-3 rounded-circle' />
+          ) : (
+            <div className='mr-3'>
+              <svg width='90' height='90'>
+                <circle cx='45' cy='45' r='45' fill='#D5D8DC' />
+                Sorry, your browser does not support inline SVG.
+              </svg>{' '}
+            </div>
+          )}
+          <div className='media-body'>
+            <h5 className='mt-0 mb-1'>{crewMember.name}</h5>
+            {crewMember.job}
+          </div>
+        </li>
+      </Fragment>
+    ))
+    return importantCrewMembers
+  }
+
   getCast = () => {
     const castImageBase = 'https://image.tmdb.org/t/p/w90_and_h90_face'
     const castArray = this.state.data.credits.cast
     const cast = castArray.slice(0, this.state.displayedCastMembers).map(castMember => (
-      <Fragment key={castMember.id}>
+      <Fragment key={castMember.id + castMember.character}>
         <li className='media my-3'>
           {castMember.profile_path ? (
             <img alt={castMember.name} src={castImageBase + castMember.profile_path} className='mr-3 rounded-circle' />
@@ -124,68 +163,76 @@ class MovieDetails extends Component {
 
   render() {
     let bgImage = this.state.dataIsReady
-      ? 'url(https://image.tmdb.org/t/p/w1280' + this.getBackground() + ')'
+      ? 'linear-gradient(rgba(52,58,64,.6), rgba(52,58,64,.6)), url(https://image.tmdb.org/t/p/w1280' +
+        this.getBackground() +
+        ')'
       : 'url(data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==)'
     return (
       <Fragment>
-        {this.state.isOpened ? (
-          <div className='row'>
-            {this.state.dataIsReady ? (
-              <div className='col'>
+        {this.state.dataIsReady ? (
+          <div className='container'>
+            <header border-bottom='1px' solid='#000'>
+              <h2 className='display-4 mt-2' id='movieDetailsLabel' display='inline' style={{ lineHeight: '.8em' }}>
+                {this.getTitle()}
+                <span className='lead' style={{ lineHeight: '.8em' }}>
+                  {' '}
+                  ({this.getReleaseYear()}){' '}
+                </span>
+              </h2>
+            </header>
+            <blockquote className='blockquote-footer lead'>{this.getTagline()}</blockquote>
+            <div
+              className='row text-white'
+              style={{
+                backgroundImage: bgImage,
+                backgroundRepeat: 'no-repeat',
+                backgroundSize: 'cover',
+                filter: 'grayscale(100%)'
+              }}>
+              <div className='col-md-3 my-3'>
+                <img src={this.getPoster()} alt='poster' style={{ width: '100%' }} />
+              </div>
+              <div className='col m-4'>
                 <div>
-                  <div className='container'>
-                    <header border-bottom='1px' solid='#000'>
-                      <h2 className='display-4' id='movieDetailsLabel' display='inline'>
-                        {this.getTitle()}
-                        <span className='lead'> ({this.getReleaseYear()}) </span>
-                      </h2>
-                    </header>
-                    <blockquote className='blockquote-footer lead'>{this.getTagline()}</blockquote>
-                    <div
-                      className='row text-white'
-                      style={{
-                        backgroundImage: bgImage,
-                        backgroundRepeat: 'no-repeat',
-                        filter: 'grayscale(100%)'
-                      }}>
-                      <div>
-                        <img src={this.getPoster()} alt='poster' className='img-fluid' height='200px' />
-                      </div>
-                      <div className='col my-1'>
-                        <h4>Overview:</h4>
-                        {this.getOverview()}
-                        <br />
-                      </div>
-                    </div>
-                    <div className='row'>
-                      <div className='col-md-3 my-3'>
-                        {this.getCompanyLogos()}
-                        <br />
-                        <strong>Company:</strong> {this.getCompanies()}
-                        <br />
-                        <strong>Duration:</strong> {this.getRuntime()} mins
-                        <br />
-                        <strong>Genre:</strong> {this.getGenres()}
-                        <br />
-                      </div>
-                      <div className='col my-3'>
-                        <h4>Cast:</h4>
-                        <ul className='list-unstyled'>{this.getCast()}</ul>
-                        {!this.state.fullCastIsOpened ? (
-                          <button className='btn btn-dark' onClick={this.setDisplayedCast}>
-                            Show full cast
-                          </button>
-                        ) : (
-                          <button className='btn btn-dark' onClick={this.setBackDisplayedCast}>
-                            Hide full cast
-                          </button>
-                        )}
-                      </div>
-                    </div>
-                  </div>
+                  <h4>Overview:</h4>
+                  <p className='mb-2'>{this.getOverview()}</p>
+                </div>
+                <h4>Creators:</h4>
+                <div className='row'>
+                  <ul className='row list-unstyled list-group list-group-horizontal'>{this.getCrew()}</ul>
                 </div>
               </div>
-            ) : null}
+            </div>
+            <div className='row'>
+              <div className='col-md-3 my-3'>
+                <h4>Facts:</h4>
+                {this.getCompanyLogos()}
+                <br />
+                <strong>Company:</strong> {this.getCompanies()}
+                <br />
+                <strong>Duration:</strong> {this.getRuntime()} mins
+                <br />
+                <strong>Genre:</strong> {this.getGenres()}
+                <br />
+                <strong>Release:</strong> {this.getReleaseDate()}
+                <br />
+                <strong>Voted:</strong> ★{this.getVotes()}/10
+                <br />
+              </div>
+              <div className='col my-3'>
+                <h4>Cast:</h4>
+                <ul className='list-unstyled'>{this.getCast()}</ul>
+                {!this.state.fullCastIsOpened ? (
+                  <button className='btn btn-dark' onClick={this.setDisplayedCast}>
+                    Show full cast
+                  </button>
+                ) : (
+                  <button className='btn btn-dark' onClick={this.setBackDisplayedCast}>
+                    Hide full cast
+                  </button>
+                )}
+              </div>
+            </div>
           </div>
         ) : null}
       </Fragment>
