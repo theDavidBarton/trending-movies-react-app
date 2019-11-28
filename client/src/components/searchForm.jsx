c5f448dd5b44171beefb1c38614984a82a31681a
David Barton <barton.david.sv@gmail.com>
2019-11-28 11:30:56 +0100

    finetune autocomplete sitebuild

--- client/src/components/searchForm.jsx
+++ client/src/components/searchForm.jsx
@@ -22,47 +22,36 @@ class SearchForm extends Component {
     }
   }
 
-  parseJson = () => {
-    const response = this.state.data.results[0]
-    console.log(response)
-  }
-
-  getTitle = () => {
-    const title = this.state.data.results[0].title
-    console.log(this.state.data)
-    return title
-  }
-
-  getReleaseYear = () => {
-    const releaseYear = this.state.data.results[0].release_date.match(/[0-9]{4}/)
-    return releaseYear
-  }
-
-  selectedMovie = () => {
-    const movie = '/movie/' + this.state.response.results[0].id
-    return movie
-  }
-
   getDropdown = () => {
     const dropdown = (
       <Fragment>
         {this.state.data.total_results >= 1 ? (
           this.state.data.results.slice(0, 5).map(result => (
             <a key={result.id + 'a'} href={`/movie/${result.id}`} className='text-decoration-none'>
-              <li key={result.id + 'li'} className='my-1'>
-                <img
-                  alt={result.title}
-                  key={result.id + 'img'}
-                  src={result.poster_path ? `https://image.tmdb.org/t/p/w45${result.poster_path}` : null}
-                />
+              <li
+                key={result.id + 'li'}
+                className='my-1 text-nowrap d-inline-block text-truncate'
+                style={{ maxWidth: '365px' }}>
+                {result.poster_path ? (
+                  <img
+                    alt={result.title}
+                    key={result.id + 'img'}
+                    src={`https://image.tmdb.org/t/p/w45${result.poster_path}`}
+                  />
+                ) : (
+                  <svg width='45' height='62'>
+                    <circle cx='45' cy='45' r='45' fill='#D5D8DC' />
+                    Sorry, your browser does not support inline SVG.
+                  </svg>
+                )}
                 <span key={result.id + 'span'} className='mx-1'>
-                  {result.title} ({result.release_date.match(/[0-9]{4}/)})
+                  {result.title} ({result.release_date ? result.release_date.match(/[0-9]{4}/) : 'unknown'})
                 </span>
               </li>
             </a>
           ))
         ) : (
-          <p>no results found...</p>
+          <p className='my-1'>no results found...</p>
         )}
       </Fragment>
     )
@@ -72,31 +61,37 @@ class SearchForm extends Component {
   setKeywordInInput = event => {
     this.setState({ keyword: event.target.value })
     if (event.target.value.length > 3) {
-      this.getTmdbApi() // save some requests from sending during typing
+      this.getTmdbApi() // send request to api only after 3 characters
       this.setState({ dropdownIsopened: true })
     }
-    console.log(this.state.keyword)
+    console.log(this.state.keyword) // _TODO: remove this before merge
+  }
+
+  closeDropdown = () => {
+    this.setState({ dropdownIsopened: false })
   }
 
   render() {
     return (
       <Fragment>
-        <input
-          className='form-control'
-          type='text'
-          placeholder='Type a movie name…'
-          value={this.state.keyword}
-          onChange={this.setKeywordInInput}
-        />
-        {this.state.dataIsReady ? (
-          <Fragment>
-            {this.state.dropdownIsopened ? (
-              <div className='bg-light text-dark position-absolute py-2 px-2' style={{ zIndex: 1 }}>
-                <ul className='list-unstyled'>{this.getDropdown()}</ul>
-              </div>
-            ) : null}
-          </Fragment>
-        ) : null}
+        <div>
+          <input
+            className='form-control'
+            type='text'
+            placeholder='Type a movie name…'
+            value={this.state.keyword}
+            onChange={this.setKeywordInInput}
+          />
+          {this.state.dataIsReady ? (
+            <Fragment>
+              {this.state.dropdownIsopened ? (
+                <div className='bg-light w-auto text-dark position-absolute py-2 px-2' style={{ zIndex: 1 }}>
+                  <ul className='list-unstyled'>{this.getDropdown()}</ul>
+                </div>
+              ) : null}
+            </Fragment>
+          ) : null}
+        </div>
       </Fragment>
     )
   }
